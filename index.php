70072430d3c79995dc7765d15d67a72984911c52
Nathan Fillion <NathanFillion@users.noreply.github.com>
2016-10-20 16:59:40 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+	session_start();
+?>
+<html>
+	<head>
+<?php
+		require_once("templates/header.php");
+		require_once("templates/navbar.php");
+?>
+	</head>
+	<body class = "mainbody">
+		
+	</body>
+</html>
+<?php
+	#require_once("templates/footer.php");
+?>
